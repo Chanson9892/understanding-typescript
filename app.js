e55a530a68ff55ae116b4bd32d6aee37e507f755
Chanson9892 <chan.hanson5@gmail.com>
2021-06-28 22:21:52 -0700

    non typescript way to get an error if type is not number

--- app.js
+++ app.js
@@ -1,4 +1,8 @@
 function add(n1, n2) {
+    console.log(typeof (n1)); // typeof lets you know the type of variable
+    if (typeof (n1) !== 'number' || typeof (n2) !== 'number') { // non typescript way to not run it if they are not numbers
+        throw new Error("incorrect input");
+    }
     return n1 + n2;
 }
 var number1 = '5';
--- app.ts
+++ app.ts
@@ -1,4 +1,9 @@
 function add(n1: number, n2: number) { // added type assignment, both parameters should be number. other types not allowed
+    console.log(typeof(n1)) // typeof lets you know the type of variable
+
+    if (typeof(n1) !== 'number' || typeof(n2) !== 'number') { // non typescript way to not run it if they are not numbers
+        throw new Error ("incorrect input")
+    }
     return n1 + n2
 }
 
