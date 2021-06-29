4dfd32484b6d5224f96922c6a7ee22249d0d80db
Chanson9892 <chan.hanson5@gmail.com>
2021-06-28 22:57:08 -0700

    learning about TS objects

--- app.js
+++ app.js
@@ -1,3 +1,7 @@
+// const person: {
+//     name: string, // explicitly tell the types
+//     age: number
+// } = { // TS specialized object type where we provide some info about structure of object, doesn't create new JS object
 var person = {
     name: "Chandler",
     age: 23
--- app.ts
+++ app.ts
@@ -1,4 +1,9 @@
-const person = {
+// const person: {
+//     name: string, // explicitly tell the types
+//     age: number
+// } = { // TS specialized object type where we provide some info about structure of object, doesn't create new JS object
+
+const person = { // Infers type is better
     name: "Chandler",
     age: 23,
 
