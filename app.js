3662c540bd846b376c8a824321edb757b31e9b99
Chanson9892 <chan.hanson5@gmail.com>
2021-07-06 17:26:06 -0700

    arrays can have certain types that make it easier to modify

--- app.js
+++ app.js
@@ -4,6 +4,14 @@
 // } = { // TS specialized object type where we provide some info about structure of object, doesn't create new JS object
 var person = {
     name: "Chandler",
-    age: 23
+    age: 23,
+    hobbies: ['sports', 'video games']
 };
-console.log(person);
+var favoriteActivities; // lets them know its an array of strings
+favoriteActivities = ['sports'];
+// let favoriteActivities: any[] // create an array with strings and numbers and whatever else
+console.log(person.name);
+for (var _i = 0, _a = person.hobbies; _i < _a.length; _i++) {
+    var hobby = _a[_i];
+    console.log(hobby);
+}
--- app.ts
+++ app.ts
@@ -6,7 +6,18 @@
 const person = { // Infers type is better
     name: "Chandler",
     age: 23,
+    hobbies: ['sports', 'video games']
 
 }
 
-console.log(person)
+let favoriteActivities: string[] // lets them know its an array of strings
+favoriteActivities = ['sports']
+
+// let favoriteActivities: any[] // create an array with strings and numbers and whatever else
+
+console.log(person.name)
+
+for (const hobby of person.hobbies) {
+    console.log(hobby.toUpperCase()) // typescript inference knows the array is only strings so we can do .toUpperCase
+    // console.log(hobby.map()) !!!! error map is for arrays but not type string
+}
