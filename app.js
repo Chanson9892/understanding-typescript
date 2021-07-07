bdf1466d77069672a4306a794a6c6c6856b0080f
Chanson9892 <chan.hanson5@gmail.com>
2021-07-07 14:49:19 -0700

    can use literal types with union types for arguments to ensure only a couple very specific arguments

--- app.js
+++ app.js
@@ -1,14 +1,28 @@
-function combine(input1, input2) {
+function combine(input1, input2, resultConversion) {
     var result;
-    if (typeof (input1) === 'number' && typeof (input2) === 'number') { // work with nums
-        result = input1 + input2;
+    // runtime type check might be necessary for union types
+    // if (typeof(input1) === 'number' && typeof(input2) === 'number') { // work with nums
+    //     result = input1 + input2
+    // } else {
+    //     result = input1.toString() + input2.toString() // work with str
+    // }
+    // if (resultConversion === 'as-number') { // literal types
+    //     return +result // converts it to number or use parseFloat(result)
+    // } else {
+    //     return result.toString()
+    // }
+    // converting the strings to numbers before adding the values if the resultConversion is number
+    if (typeof (input1) === 'number' && typeof (input2) === 'number' || resultConversion === 'as-number') { // work with nums
+        result = +input1 + +input2; // each input converted to number
     }
     else {
         result = input1.toString() + input2.toString(); // work with str
     }
     return result;
 }
-var combinedAges = combine(20, 23);
+var combinedAges = combine(20, 23, 'as-number');
 console.log(combinedAges);
-var combinedNames = combine("Ben", "Jammin");
+var combinedStringAges = combine('20', '23', 'as-number');
+console.log(combinedStringAges);
+var combinedNames = combine("Ben", "Jammin", 'as-text');
 console.log(combinedNames);
--- app.ts
+++ app.ts
@@ -1,18 +1,35 @@
-function combine(input1: number | string, input2: number | string) { 
+function combine(input1: number | string, input2: number | string, resultConversion: 'as-number' | 'as-text') { 
 
     let result;
 
-    if (typeof(input1) === 'number' && typeof(input2) === 'number') { // work with nums
-        result = input1 + input2
+    // runtime type check might be necessary for union types
+    // if (typeof(input1) === 'number' && typeof(input2) === 'number') { // work with nums
+    //     result = input1 + input2
+    // } else {
+    //     result = input1.toString() + input2.toString() // work with str
+    // }
+
+    // if (resultConversion === 'as-number') { // literal types
+    //     return +result // converts it to number or use parseFloat(result)
+    // } else {
+    //     return result.toString()
+    // }
+
+    // converting the strings to numbers before adding the values if the resultConversion is number
+    if (typeof(input1) === 'number' && typeof(input2) === 'number' || resultConversion === 'as-number') { // work with nums
+        result = +input1 + +input2 // each input converted to number
     } else {
         result = input1.toString() + input2.toString() // work with str
     }
-
     return result
+    
 }
 
-const combinedAges = combine(20, 23)
+const combinedAges = combine(20, 23, 'as-number')
 console.log(combinedAges)
 
-const combinedNames = combine("Ben", "Jammin")
+const combinedStringAges = combine('20', '23', 'as-number')
+console.log(combinedStringAges)
+
+const combinedNames = combine("Ben", "Jammin", 'as-text')
 console.log(combinedNames)
