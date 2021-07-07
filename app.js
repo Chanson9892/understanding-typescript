6fc0e050d90ec04542a27f6d95220c14bcbb2f9b
Chanson9892 <chan.hanson5@gmail.com>
2021-07-06 18:06:03 -0700

    enums gives us constant identifiers to use that can give us a value. kind of like key value pairs

--- app.js
+++ app.js
@@ -1,17 +1,39 @@
 // const person: {
 //     name: string, // explicitly tell the types
 //     age: number
+//     hobbies: string[]
+//     role: [number, string]
 // } = { // TS specialized object type where we provide some info about structure of object, doesn't create new JS object
+// const person = { // Infers type is better
+//     name: "Chandler",
+//     age: 23,
+//     hobbies: ['sports', 'video games'],
+//     role: [2, 'author'] // tuple example, for this example, identifiers are number or string
+// }
+// let favoriteActivities: string[] // lets them know its an array of strings
+// favoriteActivities = ['sports']
+// // let favoriteActivities: any[] // create an array with strings and numbers and whatever else
+// console.log(person.name)
+// for (const hobby of person.hobbies) {
+//     console.log(hobby.toUpperCase()) // typescript inference knows the array is only strings so we can do .toUpperCase
+//     // console.log(hobby.map()) !!!! error map is for arrays but not type string
+// }
+////////////////////////////////////////////////////////////////////////////////////////// enum
+// const ADMIN = 0
+// const READ_ONLY = 1
+// const AUTHOR = 2
+var Role;
+(function (Role) {
+    Role[Role["ADMIN"] = 0] = "ADMIN";
+    Role[Role["READ_ONLY"] = 1] = "READ_ONLY";
+    Role[Role["AUTHOR"] = 2] = "AUTHOR";
+})(Role || (Role = {})); // assigns labels to numbers
 var person = {
     name: "Chandler",
     age: 23,
-    hobbies: ['sports', 'video games']
+    hobbies: ['sports', 'video games'],
+    role: Role.ADMIN // enum
 };
-var favoriteActivities; // lets them know its an array of strings
-favoriteActivities = ['sports'];
-// let favoriteActivities: any[] // create an array with strings and numbers and whatever else
-console.log(person.name);
-for (var _i = 0, _a = person.hobbies; _i < _a.length; _i++) {
-    var hobby = _a[_i];
-    console.log(hobby);
+if (person.role === Role.ADMIN) {
+    console.log('is admin');
 }
--- app.ts
+++ app.ts
@@ -5,23 +5,43 @@
 //     role: [number, string]
 // } = { // TS specialized object type where we provide some info about structure of object, doesn't create new JS object
 
-const person = { // Infers type is better
+// const person = { // Infers type is better
+//     name: "Chandler",
+//     age: 23,
+//     hobbies: ['sports', 'video games'],
+//     role: [2, 'author'] // tuple example, for this example, identifiers are number or string
+
+// }
+
+
+// let favoriteActivities: string[] // lets them know its an array of strings
+// favoriteActivities = ['sports']
+
+// // let favoriteActivities: any[] // create an array with strings and numbers and whatever else
+
+// console.log(person.name)
+
+// for (const hobby of person.hobbies) {
+//     console.log(hobby.toUpperCase()) // typescript inference knows the array is only strings so we can do .toUpperCase
+//     // console.log(hobby.map()) !!!! error map is for arrays but not type string
+// }
+
+////////////////////////////////////////////////////////////////////////////////////////// enum
+
+// const ADMIN = 0
+// const READ_ONLY = 1
+// const AUTHOR = 2
+
+enum Role { ADMIN, READ_ONLY, AUTHOR } // assigns labels to numbers. admin = 0, read_only = 1, author = 2 but can set admin = 3
+
+const person = { 
     name: "Chandler",
     age: 23,
     hobbies: ['sports', 'video games'],
-    role: [2, 'author'] // tuple example, for this example, identifiers are number or string
+    role: Role.ADMIN // enum
 
 }
 
-
-let favoriteActivities: string[] // lets them know its an array of strings
-favoriteActivities = ['sports']
-
-// let favoriteActivities: any[] // create an array with strings and numbers and whatever else
-
-console.log(person.name)
-
-for (const hobby of person.hobbies) {
-    console.log(hobby.toUpperCase()) // typescript inference knows the array is only strings so we can do .toUpperCase
-    // console.log(hobby.map()) !!!! error map is for arrays but not type string
+if (person.role === Role.ADMIN){
+    console.log('is admin')
 }
