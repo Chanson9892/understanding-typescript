e3e71e88f482bba468380e1dcda5242bfc77f651
Chanson9892 <chan.hanson5@gmail.com>
2021-07-06 18:47:11 -0700

    moved object, array, enum practice to objArrayEnum.ts, working on union types in app

--- app.js
+++ app.js
@@ -1,39 +1,14 @@
-// const person: {
-//     name: string, // explicitly tell the types
-//     age: number
-//     hobbies: string[]
-//     role: [number, string]
-// } = { // TS specialized object type where we provide some info about structure of object, doesn't create new JS object
-// const person = { // Infers type is better
-//     name: "Chandler",
-//     age: 23,
-//     hobbies: ['sports', 'video games'],
-//     role: [2, 'author'] // tuple example, for this example, identifiers are number or string
-// }
-// let favoriteActivities: string[] // lets them know its an array of strings
-// favoriteActivities = ['sports']
-// // let favoriteActivities: any[] // create an array with strings and numbers and whatever else
-// console.log(person.name)
-// for (const hobby of person.hobbies) {
-//     console.log(hobby.toUpperCase()) // typescript inference knows the array is only strings so we can do .toUpperCase
-//     // console.log(hobby.map()) !!!! error map is for arrays but not type string
-// }
-////////////////////////////////////////////////////////////////////////////////////////// enum
-// const ADMIN = 0
-// const READ_ONLY = 1
-// const AUTHOR = 2
-var Role;
-(function (Role) {
-    Role[Role["ADMIN"] = 0] = "ADMIN";
-    Role[Role["READ_ONLY"] = 1] = "READ_ONLY";
-    Role[Role["AUTHOR"] = 2] = "AUTHOR";
-})(Role || (Role = {})); // assigns labels to numbers
-var person = {
-    name: "Chandler",
-    age: 23,
-    hobbies: ['sports', 'video games'],
-    role: Role.ADMIN // enum
-};
-if (person.role === Role.ADMIN) {
-    console.log('is admin');
+function combine(input1, input2) {
+    var result;
+    if (typeof (input1) === 'number' && typeof (input2) === 'number') { // work with nums
+        result = input1 + input2;
+    }
+    else {
+        result = input1.toString() + input2.toString(); // work with str
+    }
+    return result;
 }
+var combinedAges = combine(20, 23);
+console.log(combinedAges);
+var combinedNames = combine("Ben", "Jammin");
+console.log(combinedNames);
--- app.ts
+++ app.ts
@@ -1,47 +1,18 @@
-// const person: {
-//     name: string, // explicitly tell the types
-//     age: number
-//     hobbies: string[]
-//     role: [number, string]
-// } = { // TS specialized object type where we provide some info about structure of object, doesn't create new JS object
+function combine(input1: number | string, input2: number | string) { 
 
-// const person = { // Infers type is better
-//     name: "Chandler",
-//     age: 23,
-//     hobbies: ['sports', 'video games'],
-//     role: [2, 'author'] // tuple example, for this example, identifiers are number or string
+    let result;
 
-// }
+    if (typeof(input1) === 'number' && typeof(input2) === 'number') { // work with nums
+        result = input1 + input2
+    } else {
+        result = input1.toString() + input2.toString() // work with str
+    }
 
-
-// let favoriteActivities: string[] // lets them know its an array of strings
-// favoriteActivities = ['sports']
-
-// // let favoriteActivities: any[] // create an array with strings and numbers and whatever else
-
-// console.log(person.name)
-
-// for (const hobby of person.hobbies) {
-//     console.log(hobby.toUpperCase()) // typescript inference knows the array is only strings so we can do .toUpperCase
-//     // console.log(hobby.map()) !!!! error map is for arrays but not type string
-// }
-
-////////////////////////////////////////////////////////////////////////////////////////// enum
-
-// const ADMIN = 0
-// const READ_ONLY = 1
-// const AUTHOR = 2
-
-enum Role { ADMIN, READ_ONLY, AUTHOR } // assigns labels to numbers. admin = 0, read_only = 1, author = 2 but can set admin = 3
-
-const person = { 
-    name: "Chandler",
-    age: 23,
-    hobbies: ['sports', 'video games'],
-    role: Role.ADMIN // enum
-
+    return result
 }
 
-if (person.role === Role.ADMIN){
-    console.log('is admin')
-}
+const combinedAges = combine(20, 23)
+console.log(combinedAges)
+
+const combinedNames = combine("Ben", "Jammin")
+console.log(combinedNames)
