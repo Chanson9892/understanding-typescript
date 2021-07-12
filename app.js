d2dd45f1bc9027a6c016cb143858702777032ba6
Chanson9892 <chan.hanson5@gmail.com>
2021-07-12 15:56:36 -0700

    we can store a function in a variable that has specific types for arguments and return value expected

--- app.js
+++ app.js
@@ -1,28 +1,10 @@
-function combine(input1, input2, resultConversion) {
-    var result;
-    // runtime type check might be necessary for union types
-    // if (typeof(input1) === 'number' && typeof(input2) === 'number') { // work with nums
-    //     result = input1 + input2
-    // } else {
-    //     result = input1.toString() + input2.toString() // work with str
-    // }
-    // if (resultConversion === 'as-number') { // literal types
-    //     return +result // converts it to number or use parseFloat(result)
-    // } else {
-    //     return result.toString()
-    // }
-    // converting the strings to numbers before adding the values if the resultConversion is number
-    if (typeof (input1) === 'number' && typeof (input2) === 'number' || resultConversion === 'as-number') { // work with nums
-        result = +input1 + +input2; // each input converted to number
-    }
-    else {
-        result = input1.toString() + input2.toString(); // work with str
-    }
-    return result;
+function add(n1, n2) {
+    return n1 + n2;
 }
-var combinedAges = combine(20, 23, 'as-number');
-console.log(combinedAges);
-var combinedStringAges = combine('20', '23', 'as-number');
-console.log(combinedStringAges);
-var combinedNames = combine("Ben", "Jammin", 'as-text');
-console.log(combinedNames);
+function printResult(num) {
+    console.log("Result: " + num);
+}
+printResult(add(5, 12));
+var combinedValues;
+combinedValues = add;
+console.log(combinedValues(8, 8));
--- app.ts
+++ app.ts
@@ -1,3 +1,15 @@
-function add(n1: number, n2: number){
-    
+function add(n1: number, n2: number): number { // functions have return types. can set it or infer it
+    return n1 + n2
 }
+
+function printResult(num: number){ // the return type is void because we are not returning anything
+    console.log("Result: " + num)
+}
+
+printResult(add(5, 12))
+
+// we can store a function in a variable. the paranthesis are arguments the right is return
+let combineValues: (a: number, b:number) => number; 
+combineValues = add;
+console.log(combineValues(8,8))
+
